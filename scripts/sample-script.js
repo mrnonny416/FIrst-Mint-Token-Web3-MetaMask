f883e25cbba55e7e0ac6add5830abb38abe81811
mrnonny416 <mrnonny416@gmail.com>
2022-03-28 18:10:56 +0700

    First Commit

---
+++ scripts/sample-script.js
@@ -0,0 +1,19 @@
+
+const hre = require("hardhat");
+
+async function main() {
+
+  const MintExample = await hre.ethers.getContractFactory("MintExample");
+  const mintExample = await MintExample.deploy("Minter", "MINT");
+
+  await mintExample.deployed();
+
+  console.log("MintExample deployed to:", mintExample.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
